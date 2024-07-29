e4dc17996be3334af37b8d9635c241c3a981f53a
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2024-07-29 09:36:53 +0800

    Add buffer

--- main.js
+++ main.js
@@ -1,52 +1,11 @@
-// const opts = {
-//    angle: -0.3, // The span of the gauge arc
-//    lineWidth: 0.13, // The line thickness
-//    radiusScale: 1, // Relative radius
-//    pointer: {
-//       length: 0.46, // // Relative to gauge radius
-//       strokeWidth: 0.045, // The thickness
-//       color: '#051623', // Fill color
-//    },
-//    limitMax: false,     // If false, max value increases automatically if value > maxValue
-//    limitMin: false,     // If true, the min value of the gauge will be fixed
-//    colorStart: '#76D8FF',   // Colors
-//    colorStop: '#1B36FF',    // just experiment with them
-//    strokeColor: '#EEEEEE',  // to see which ones work best for you
-//    generateGradient: true,
-//    highDpiSupport: true,     // High resolution support
-//    // renderTicks is Optional
-//    staticLabels: {
-//       font: "16px Montserrat",  // Specifies font
-//       labels: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],  // Print labels at these values
-//       color: "#000000",  // Optional: Label text color
-//       fractionDigits: 0  // Optional: Numerical precision. 0=round off.
-//    },
-//    renderTicks: {
-//       divisions: 5,
-//       divWidth: 0.6,
-//       divLength: 0.6,
-//       divColor: '#fff',
-//       subDivisions: 3,
-//       subLength: 0.3,
-//       subWidth: 0.3,
-//       subColor: '#fff'
-//    }
-// };
-// const target = document.getElementById('foo'); // your canvas element
-// const gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-// gauge.maxValue = 1000; // set max gauge value
-// gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-// gauge.animationSpeed = 32; // set animation speed (32 is default value)
-// gauge.set(500); // set actual value
-// gauge.setTextField(document.getElementById("preview-textfield"));
+import SpeedTest from '@cloudflare/speedtest';
 
-/* WORKING CODE ###################
 const opts = {
    angle: -0.3, // The span of the gauge arc
    lineWidth: 0.13, // The line thickness
    radiusScale: 1, // Relative radius
    pointer: {
-      length: 0.46, // // Relative to gauge radius
+      length: 0.46, // Relative to gauge radius
       strokeWidth: 0.045, // The thickness
       color: '#051623', // Fill color
    },
@@ -59,7 +18,7 @@ const opts = {
    highDpiSupport: true,     // High resolution support
    staticLabels: {
       font: "16px Montserrat",  // Specifies font
-      labels: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],  // Print labels at these values
+      labels: [0, 200, 400, 600, 800, 1000],  // Print labels at these values
       color: "#000000",  // Optional: Label text color
       fractionDigits: 0  // Optional: Numerical precision. 0=round off.
    },
@@ -74,162 +33,95 @@ const opts = {
       subColor: '#fff'
    }
 };
+
+const startTestBtn = document.getElementById('start-test');
+const resultDiv = document.getElementById('result');
+
 const target = document.getElementById('foo'); // your canvas element
 const gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+
 gauge.maxValue = 1000; // set max gauge value
 gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
 gauge.animationSpeed = 32; // set animation speed (32 is default value)
 gauge.set(0); // initialize gauge to 0
 gauge.setTextField(document.getElementById("preview-textfield"));
 
-const startTestBtn = document.getElementById('start-test');
-const resultDiv = document.getElementById('result');
-
-const testFileUrl = 'https://staging-falconfinet.azurewebsites.net/wp-content/uploads/2024/07/hero-bg-1.webp'; // URL of the test file
-const testFileSizeInBytes = 24268.8; // Size of the test file in bytes (example: 1MB)
-
-const calculateSpeed = (startTime, endTime, fileSize) => {
-   const durationInSeconds = (endTime - startTime) / 1000;
-   const bitsLoaded = fileSize * 8;
-   const speedBps = bitsLoaded / durationInSeconds;
-   const speedKbps = speedBps / 1024;
-   const speedMbps = speedKbps / 1024;
-   return speedMbps;
+const randomizer = (min, max) => {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const testSpeed = async () => {
-   resultDiv.textContent = 'Testing...';
-   gauge.set(0); // reset gauge to 0
-   const startTime = new Date().getTime();
+startTestBtn.addEventListener("click", () => {
+   gauge.set(0);
+   startTestBtn.disabled = true;
 
-   try {
-      await fetch(testFileUrl, { cache: 'no-cache' });
-      const endTime = new Date().getTime();
-      const speedMbps = calculateSpeed(startTime, endTime, testFileSizeInBytes);
-      gauge.set(speedMbps); // update gauge with speed
-      resultDiv.textContent = `Download Speed: ${speedMbps.toFixed(2)} Mbps`;
-   } catch (error) {
-      resultDiv.textContent = 'Error: Unable to test speed.';
+   const engine = new SpeedTest();
+
+   engine.onResultsChange = () => {
+      if (!engine.isFinished) {
+         const { results } = engine.results.raw.download;
+
+         Object.values(results).forEach(({ timings }) => {
+            timings.forEach(({ bps }) => {
+               const downloadMbps = bps / 1024 / 1024;
+
+               const currentReading = randomizer(
+                  downloadMbps - 10,
+                  downloadMbps + 10
+               );
+
+               console.log(currentReading);
+               gauge.set(currentReading);
+
+               resultDiv.textContent = `Download Speed: ${
+                  currentReading.toFixed(2)
+               } Mbps`;
+            });
+         });
+      }
+   };
+
+   engine.onFinish = results => {
+      const { download } = results.getSummary();
+      const downloadMbps = download / 1024 / 1024;
+
+      gauge.set(downloadMbps);
+      resultDiv.textContent = `Download Speed: ${downloadMbps.toFixed(2)} Mbps`;
+      startTestBtn.disabled = false;
    }
-};
+});
 
-startTestBtn.addEventListener('click', testSpeed);
+// document.getElementById("start-test").addEventListener("click", () => {
+//    const engine = new SpeedTest();
 
- */
+//    engine.onResultsChange = ({ type }) => {
+//       console.log(type);
 
-// const opts = {
-//    angle: -0.3, // The span of the gauge arc
-//    lineWidth: 0.13, // The line thickness
-//    radiusScale: 1, // Relative radius
-//    pointer: {
-//       length: 0.46, // Relative to gauge radius
-//       strokeWidth: 0.045, // The thickness
-//       color: '#051623', // Fill color
-//    },
-//    limitMax: false,     // If false, max value increases automatically if value > maxValue
-//    limitMin: false,     // If true, the min value of the gauge will be fixed
-//    colorStart: '#76D8FF',   // Colors
-//    colorStop: '#1B36FF',    // just experiment with them
-//    strokeColor: '#EEEEEE',  // to see which ones work best for you
-//    generateGradient: true,
-//    highDpiSupport: true,     // High resolution support
-//    staticLabels: {
-//       font: "16px Montserrat",  // Specifies font
-//       labels: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],  // Print labels at these values
-//       color: "#000000",  // Optional: Label text color
-//       fractionDigits: 0  // Optional: Numerical precision. 0=round off.
-//    },
-//    renderTicks: {
-//       divisions: 5,
-//       divWidth: 0.6,
-//       divLength: 0.6,
-//       divColor: '#fff',
-//       subDivisions: 3,
-//       subLength: 0.3,
-//       subWidth: 0.3,
-//       subColor: '#fff'
-//    }
-// };
-// const target = document.getElementById('foo'); // your canvas element
-// const gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-// gauge.maxValue = 1000; // set max gauge value
-// gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-// gauge.animationSpeed = 32; // set animation speed (32 is default value)
-// gauge.set(0); // initialize gauge to 0
-// gauge.setTextField(document.getElementById("preview-textfield"));
+//       if (!engine.isFinished) {
+//          // console.log(engine.results.raw);
 
-// const startTestBtn = document.getElementById('start-test');
-// const resultDiv = document.getElementById('result');
+//          const { results } = engine.results.raw.download;
 
-// const testFileUrl = 'https://staging-falconfinet.azurewebsites.net/wp-content/uploads/2024/07/download-image.jpeg'; // URL of the test file
-// const testFileSizeInBytes = 4737536; // Size of the test file in bytes (example: 2MB)
+//          console.log(results);
 
-// const calculateSpeed = (startTime, endTime, fileSize) => {
-//    const durationInSeconds = (endTime - startTime) / 1000;
-//    const bitsLoaded = fileSize * 8;
-//    const speedBps = bitsLoaded / durationInSeconds;
-//    const speedKbps = speedBps / 1024;
-//    const speedMbps = speedKbps / 1024;
-//    return speedMbps;
-// };
+//          Object.values(results).forEach(({ timings }) => {
+//             timings.forEach(({ bps }) => {
+//                const reading = bps / 1000000;
+//                const currentReading = randomizer(reading, reading + 10);
 
-// const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-// const testSpeed = async () => {
-//    startTestBtn.disabled = true; // Disable button
-//    resultDiv.textContent = 'Speed testing...'; // Show buffering message
-
-//    await delay(2000); // Delay of 2 seconds before starting the test
-
-//    const startTime = new Date().getTime();
-
-//    try {
-//       const response = await fetch(testFileUrl, { cache: 'no-cache' });
-//       if (!response.ok) {
-//          throw new Error(`HTTP error! status: ${response.status}`);
+//                console.log(currentReading);
+//                document.getElementById("buffer").innerHTML = currentReading;
+//             });
+//          });
 //       }
-//       const endTime = new Date().getTime();
-//       const speedMbps = calculateSpeed(startTime, endTime, testFileSizeInBytes);
-//       gauge.set(speedMbps); // update gauge with speed
-//       resultDiv.textContent = `${speedMbps.toFixed(2)} Mbps`;
-//    } catch (error) {
-//       resultDiv.textContent = `Error: Unable to test speed. ${error.message}`;
-//    } finally {
-//       startTestBtn.disabled = false; // Re-enable button
-//    }
-// };
+//    };
 
-// startTestBtn.addEventListener('click', testSpeed);
+//    engine.onFinish = (results) => {
+//       console.log(results.getSummary());
 
-// const FastSpeedtest = require("fast-speedtest-api");
+//       const { download } = results.getSummary();
+//       const result = download / 1000000;
 
-// import FastSpeedtest from "fast-speedtest-api";
-
-// let speedtest = new FastSpeedtest({
-//    token: "your-app-token", // required
-//    verbose: false, // default: false
-//    timeout: 10000, // default: 5000
-//    https: true, // default: true
-//    urlCount: 5, // default: 5
-//    bufferSize: 8, // default: 8
-//    unit: FastSpeedtest.UNITS.Mbps // default: Bps
+//       console.log(result);
+//       document.getElementById("speed-result").innerHTML = result;
+//    };
 // });
-
-// speedtest.getSpeed().then(s => {
-//    console.log(`Speed: ${s} Mbps`);
-// }).catch(e => {
-//    console.error(e.message);
-// });
-
-// const speedtest = new Speedtest();
-// speedtest.getSpeed().then(result => {
-//    console.log(`Speed: ${result} Mbps`);
-// }).catch(error => {
-//    console.error('Error testing speed:', error);
-// });
-
-// console.log(81746935.81930286 / 1024);
-
-import SpeedTest from '@cloudflare/speedtest';
-
-new SpeedTest().onFinish = results => console.log(results.getSummary());
